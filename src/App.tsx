032638636bbbdd9742ea048117a061830106d272
Yasniel <yasnielfajardoegues1408@gmail.com>
2021-11-25 09:51:25 -0500

    refactoring eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { Home } from 'pages/Home';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from 'pages/Home';
 
-function App() {
+const App = function () {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
           <Home />
         </Route>
+        <Route path="*">
+          <h1>HOLAA</h1>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
-export const Home = () => {
+const Home = () => {
   return (
     <div>
       <h1>HOLA MUNDO</h1>
     </div>
   );
 };
+
+export default Home;
